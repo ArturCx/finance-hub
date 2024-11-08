c19a8c4c260d6802aa13fcf2e025107444cd3611
ArturCx <arturcesarsilva@gmail.com>
2024-11-08 14:13:50 -0300

    feat/add navbar component

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,18 +2,34 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/dataTable";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/addTransactionButton";
+import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+//import { ScrollArea } from "../_components/ui/scroll-area";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
-    <div className="p-6 space-y-6">
-      {/*Título e Botão */}
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="space-y-6 overflow-hidden p-6">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable columns={transactionColumns} data={transactions} />
+        {/*<ScrollArea>
+        </ScrollArea>*/}
       </div>
-      <DataTable columns={transactionColumns} data={transactions} />
-    </div>
+    </>
   );
 };
 
